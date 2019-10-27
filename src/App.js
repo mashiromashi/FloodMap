5d13ae1756b17234c16c2aab9d00d432ae4bdb0f
Htin Wana <Htinwana.dev@gmail.com>
2019-10-28 02:05:36 +0630

    misc

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React, { Component } from "react";
 import M from "materialize-css";
 import "./App.css";
-
 import NavBar from "./Components/NavBar/NavBar";
-import "materialize-css";
 import Content from "./Route/content";
+
 class App extends Component {
   componentDidMount() {
     M.AutoInit();

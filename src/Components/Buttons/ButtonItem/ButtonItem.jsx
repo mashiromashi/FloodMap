ef79be5773a6f0c78816a7758daff20fd0068b22
Htin Wana <Htinwana.dev@gmail.com>
2019-11-03 12:03:39 +0630

    add pin color logic based on latest water level fetched

--- src/Components/Buttons/ButtonItem/ButtonItem.jsx
+++ src/Components/Buttons/ButtonItem/ButtonItem.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
-import { Button } from '@material-ui/core'
+import "../../../index.css"
 
 const ButtonItem = ({ buttonName, onClick }) => {
     return (
-        <Button onClick={onClick}>{buttonName}</Button>
+        <button
+            //className="btn-small waves-effect waves-light"
+            className="buttons waves-effect waves-light blue"
+            onClick={onClick}
+        >
+            {buttonName}
+        </button>
     )
 }
 

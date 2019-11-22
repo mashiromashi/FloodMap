4dfd08d39093a2059d90486c886287b2363de970
Htin Wana <htinwana.dev@gmail.com>
2019-11-22 18:05:51 +0630

    add color pin indicators below the map

--- src/Container/mapApi/MapApi.jsx
+++ src/Container/mapApi/MapApi.jsx
@@ -1,26 +1,35 @@
 /* eslint-disable no-nested-ternary */
 /* eslint-disable react/jsx-one-expression-per-line */
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import {
   GoogleMap,
   withScriptjs,
   withGoogleMap,
   Marker,
-  InfoWindow,
-} from 'react-google-maps';
-import { apiAddress } from '../../util/ApiAddresses';
+  InfoWindow
+} from "react-google-maps";
+import { apiAddress } from "../../util/ApiAddresses";
 
 function GMap() {
   const [sensors, setSensors] = useState([
     {
-      id: 1, name: 'Batasan Bridge Sensor', lat: 14.679486, lng: 121.109826, path: 'batasan', waterLevel: 0.0,
+      id: 1,
+      name: "Batasan Bridge Sensor",
+      lat: 14.679486,
+      lng: 121.109826,
+      path: "batasan",
+      waterLevel: 0.0
     },
     {
-      id: 2, name: 'Sapang Labo Sensor', lat: 14.692949, lng: 121.125996, path: 'labo', waterLevel: 0.0,
-    },
+      id: 2,
+      name: "Sapang Labo Sensor",
+      lat: 14.692949,
+      lng: 121.125996,
+      path: "labo",
+      waterLevel: 0.0
+    }
   ]);
   const [selectedSensor, setSelectedSensor] = useState(null);
-
 
   useEffect(() => {
     // eslint: sensor was never used
@@ -34,28 +43,23 @@ function GMap() {
           const newSensors = [...sensors];
           const updatedSensor = {
             ...sensor,
-            waterLevel: parseFloat(data.waterLevel.$numberDecimal),
+            waterLevel: parseFloat(data.waterLevel.$numberDecimal)
           };
           newSensors[index] = updatedSensor;
           setSensors(newSensors);
         }
       });
     }
-    const timeout = setTimeout(
-      fetchSensorData,
-      5000);
-    return () => clearTimeout(
-      timeout, 5500
-    )
+    const timeout = setTimeout(fetchSensorData, 5000);
+    return () => clearTimeout(timeout, 5500);
   });
-
 
   return (
     <GoogleMap
       defaultZoom={14}
       defaultCenter={{ lat: 14.69872, lng: 121.12606 }}
     >
-      {sensors.map((sensor) => (
+      {sensors.map(sensor => (
         <Marker
           key={sensor.id}
           position={{ lat: sensor.lat, lng: sensor.lng }}
@@ -65,19 +69,20 @@ function GMap() {
           icon={
             // this is fine
             sensor.waterLevel <= 5
-              ? 'img/cursors/greenLevel.png'
-              // a little bit not okay
-              : (sensor.waterLevel <= 10 ? 'img/cursors/yellowLevel.png' :
-                // the whole street is swimming pool
-                (sensor.waterLevel <= 20 ? 'img/cursors/orangeLevel.png' :
-                  // Our homes are gone just like my hopes and dreams
-                  (sensor.waterLevel <= 30 ? 'img/cursors/redLevel.png' :
-                    // basically atlantis
-                    (sensor.waterLevel >= 50 ? 'img/cursor/blackLevel.png' :
-                      null)
-                  )
-                )
-              )
+              ? "img/cursors/greenLevel.png"
+              : // a little bit not okay
+              sensor.waterLevel <= 10
+              ? "img/cursors/yellowLevel.png"
+              : // the whole street is swimming pool
+              sensor.waterLevel <= 20
+              ? "img/cursors/orangeLevel.png"
+              : // Our homes are gone just like my hopes and dreams
+              sensor.waterLevel <= 30
+              ? "img/cursors/redLevel.png"
+              : // basically atlantis
+              sensor.waterLevel >= 50
+              ? "img/cursor/blackLevel.png"
+              : null
           }
         />
       ))}
@@ -85,7 +90,7 @@ function GMap() {
         <InfoWindow
           position={{
             lat: selectedSensor.lat,
-            lng: selectedSensor.lng,
+            lng: selectedSensor.lng
           }}
           onCloseClick={() => {
             setSelectedSensor(null);
@@ -109,13 +114,16 @@ const WrappedMap = withScriptjs(withGoogleMap(GMap));
 
 export default function MapApi() {
   return (
-    <div style={{ width: '100vw', height: '90vh', padding: '50px' }}>
+    <div style={{ width: "100vw", height: "90vh", padding: "50px" }}>
       <WrappedMap
         googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${apiKey}`}
-        loadingElement={<div style={{ height: '100%' }} />}
-        containerElement={<div style={{ height: '100%' }} />}
-        mapElement={<div style={{ height: '100%' }} />}
+        loadingElement={<div style={{ height: "100%" }} />}
+        containerElement={<div style={{ height: "100%" }} />}
+        mapElement={<div style={{ height: "100%" }} />}
       />
+      <p className="flow-text center-align">
+        Green = 0-5m Red = 5-10m Orange = 10-15m
+      </p>
     </div>
   );
 }
--- src/util/ApiAddresses.jsx
+++ src/util/ApiAddresses.jsx
@@ -1,14 +1,18 @@
-import moment from 'moment';
-import monthArray from './month';
+import moment from "moment";
+import monthArray from "./month";
 // heroku api
-// export const apiAddress = 'https://floodmap-backend.herokuapp.com';
+export const apiAddress = "https://floodmap-backend.herokuapp.com";
 
 // localhost
-export const apiAddress = "http://localhost:8080";
-
+// export const apiAddress = "http://localhost:8080";
 
 // monthly fetch
-const inputMonth = monthArray[moment().subtract(1, 'month').month()];
+const inputMonth =
+  monthArray[
+    moment()
+      .subtract(1, "month")
+      .month()
+  ];
 export const monthlyBatasan = `${apiAddress}/batasan/monthly/q=${inputMonth}`;
 export const monthlyLabo = `${apiAddress}/labo/monthly/q=${inputMonth}`;
 
@@ -21,5 +25,5 @@ export const weeklyBatasan = `${apiAddress}/batasan/weekly`;
 export const weeklyLabo = `${apiAddress}/labo/weekly`;
 
 // current fetch
-export const currentBatasan = `${apiAddress}/batasan/current`
-export const currentLabo = `${apiAddress}/labo/current`
+export const currentBatasan = `${apiAddress}/batasan/current`;
+export const currentLabo = `${apiAddress}/labo/current`;
